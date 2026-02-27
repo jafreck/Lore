364d20709dbd0f1fc9b9db457af0c964ff3cd407
Jacob Freck <jafreck@outlook.com>
2026-02-27 12:58:40 -0800

    feat(#7): wip: Tier 2 language fixtures and tests (C, C++, C#, Dart, Elixir, Elm, Haskell, Julia) (attempt 1)

---
+++ tests/fixtures/ruby/sample.rb
@@ -0,0 +1,34 @@
+require 'json'
+require_relative 'shape'
+
+module Geometry
+  PI = Math::PI
+
+  class Circle
+    attr_reader :radius
+
+    def initialize(radius)
+      @radius = radius
+    end
+
+    def area
+      PI * @radius ** 2
+    end
+
+    def perimeter
+      2 * PI * @radius
+    end
+
+    def self.unit_circle
+      new(1.0)
+    end
+  end
+end
+
+def greet(name)
+  "Hello, #{name}!"
+end
+
+def add(a, b)
+  a + b
+end
